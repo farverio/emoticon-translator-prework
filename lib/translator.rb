b2b1b5698c6e124f54743242dff8b28311517965
Stephen F <stephen@farver.io>
2018-06-06 17:02:55 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,7 +18,7 @@ end
 
 def get_japanese_emoticon(path, western_emoticon)
   library = load_library(path)
-  library["get_emoticon"][western_emoticon]
+  if library["get_emoticon"][western_emoticon]
 end
 
 def get_english_meaning(path, japanese_emoticon)
